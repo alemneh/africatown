be34d3f0b8cb5c3226878e6dbfd3a5b0e0f699ab
Alemneh <Alemneh@hotmail.com>
2017-04-29 13:18:18 -0700

    added config dir

--- models/seeker.js
+++ models/seeker.js
@@ -3,7 +3,26 @@
 module.exports = (mongoose, models) => {
   let Schema = mongoose.Schema;
   const seekerSchema = new mongoose.Schema({
-
+    name: String,
+    email: String,
+    tel: Number,
+    gender: String,
+    dob: Date,
+    race: String,
+    profession: String,
+    income: String,
+    housingBudget: Number,
+    houseHoldSize: Number,
+    headOfHouseHold: String,
+    adultsInHouseHold: Number,
+    havePets: Boolean,
+    profilePicUrl: String,
+    zipCode: Number,
+    connectionToBlkCommunity: String,
+    vision: String,
+    barrier: String,
+    regularVistor: Boolen,
+    visitingReason: String,
   });
 
   const Seeker = mongoose.model('Seeker', seekerSchema);
